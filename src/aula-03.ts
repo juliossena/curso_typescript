4bee2144d0ff77376dd78588a5ded13114d41ed5
juliossena <juliosouzasena@gmail.com>
2023-05-11 00:19:50 -0300

    feat: aula03

---
+++ src/aula-03.ts
@@ -0,0 +1,13 @@
+interface Pessoa {
+  idade: number;
+  nome: string;
+  isMaiorIdade: boolean;
+}
+
+const pessoa: Pessoa = {
+  idade: 543,
+  nome: "fdsafa",
+  isMaiorIdade: true,
+};
+
+console.log(pessoa);
